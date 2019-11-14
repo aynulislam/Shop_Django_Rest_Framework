02dbb1c7409c34dd70492bc5bc644fe040ab73e7
Aynul Islam <41486873+aynulislam@users.noreply.github.com>
2019-11-14 15:33:47 +0600

    Add files via upload

--- order_app/urls.py
+++ order_app/urls.py
@@ -4,4 +4,9 @@ urlpatterns = [
     path('SpOrder/', views.SpOrderAPIView.as_view(), name="sp-order"),
     path('SpOrderDetail/', views.SpOrderDetailAPIView.as_view(), name="sp-delivery-boy"),
 
+    path('sp-order-detail/<int:pk>/', views.SpOrderDetail.as_view()),
+    path('je-order-list/', views.SpOrderList.as_view()),
+    path('sp-order-detail-detail/<int:pk>/', views.SpOrderDetailDetail.as_view()),
+    path('sp-order-detail-list/', views.SpOrderDetailList.as_view()),
+
 ]
